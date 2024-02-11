ebe279d011191a1c641665e9eff9c789ab2ad66f
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-02-11 13:35:22 -0300

    criando a rota de post de uma task

--- src/model.rs
+++ src/model.rs
@@ -1,12 +1,13 @@
-use serde::{Deserialize,Serialize};
-use chrono::{DateTime,Utc};
+use serde::{Deserialize, Serialize};
+use chrono::{DateTime, Utc};
 use sqlx::FromRow;
 use uuid::Uuid;
 
-#[derive(Debug,FromRow,Deserialize,Serialize)]
-pub struct TaskModel{
-    pub id:Uuid,
-    pub title:String,
-    pub content:String,
-    pub created_at:Option<DateTime<Utc>>,
+
+#[derive(Debug, FromRow, Deserialize, Serialize)]
+pub struct TaskModel {
+    pub id: Uuid,
+    pub title: String,
+    pub content: String,
+    pub created_at: Option<DateTime<Utc>>,
 }
